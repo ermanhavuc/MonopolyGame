1f5be36fbdae53df4c7cefb9d03357ead3cc6c76
sumeyragulsoy <sumeyragulsoy@yahoo.com.tr>
2017-10-26 12:13:01 +0300

    Final situation of Classes

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Scanner number;
+        int numOfPlayer = 0; // num=player sayısı
+
+        while (numOfPlayer < 2 || numOfPlayer > 8) {
+
+            System.out.print("Enter number of Players betweeen 2 and 8: ");
+            number = new Scanner(System.in);
+            numOfPlayer = number.nextInt(); // num=player sayısı
+        }
+        new MonopolyGame(numOfPlayer);
+    }
 }
