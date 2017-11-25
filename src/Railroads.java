9280d43b07fbd1e53cef49f399042267f9894c40
ermanhavuc <ermanhavuc@gmail.com>
2017-11-25 20:36:20 +0300

    Fixed Utility getOwner() error

--- src/Railroads.java
+++ src/Railroads.java
@@ -1,6 +1,7 @@
 public class Railroads extends Square {
     private int price = 200;
     private Player owner=null;
+
     public Railroads(String name, int index) {
         super(name, index);
     }
@@ -9,35 +10,36 @@ public class Railroads extends Square {
         if (getOwner()==null) {
             Die rolling = new Die();
             int roll = rolling.getFaceValue();
-            if (roll > 4 && (player.getMoney() >=getPrice())) {
+            if (roll > 4 && (player.getMoney() >= getPrice())) {
                 System.out.println(roll);
                 player.setMoney(-getPrice());
                 setOwner(player);
                 System.out.println("owner is "+getOwner().getName());
             }
         }else if (player==getOwner() ) {
-                System.out.print(getOwner().getName() +"is owner do nothing");
-                } else {
-                    Die rolling = new Die();
-                    int roll = rolling.getFaceValue();
-                    int rent = 5 * roll + 25;
-                    player.setMoney(-rent);
-                    getOwner().setMoney(rent);
-                    System.out.println(rent + " rent is paid.");
-                }
+            System.out.print(getOwner().getName() + "is owner do nothing");
+        }else {
+            Die rolling = new Die();
+            int roll = rolling.getFaceValue();
+            int rent = 5 * roll + 25;
+            player.setMoney(-rent);
+            getOwner().setMoney(rent);
+            System.out.println(rent + " rent is paid.");
         }
+    }
 
 
-    public int getPrice() {
+    private int getPrice() {
         return price;
     }
 
-    public Player getOwner() {
+    private Player getOwner() {
         return owner;
     }
 
-    public Player setOwner(Player p) {
-        return this.owner=p;
+    private void setOwner(Player p) {
+
+        owner = p;
     }
 
 }
--- src/Utility.java
+++ src/Utility.java
@@ -2,7 +2,6 @@ public class Utility extends Square {
     private int price = 150;
     private Player owner ;
 
-
     public Utility(String name, int index) {
         super(name, index);
     }
@@ -17,31 +16,29 @@ public class Utility extends Square {
                 player.setMoney(-getPrice());
                  System.out.println(getName()+"'s Owner is "+getOwner().getName()+ player.getMoney());
             }
-        }
-        if ((player == getOwner())) { // Square sahibi ile landon player aynı kişi
-           System.out.println(getOwner().getName() +"is owner do nothing");
-        } else {
-            Die rolling = new Die();
-            int roll = rolling.getFaceValue();
-            int rent = 10 * roll;
-            player.setMoney(-rent);
-            getOwner().setMoney(rent);
-            System.out.println(rent + " rent is paid to " + getOwner().getPiece().getName());
-        }
-
+        } else if ((player == getOwner())) { // Square sahibi ile landon player aynı kişi
+                System.out.println(getOwner().getName() +"is owner do nothing");
+            } else {
+                Die rolling = new Die();
+                int roll = rolling.getFaceValue();
+                int rent = 10 * roll;
+                player.setMoney(-rent);
+                getOwner().setMoney(rent);
+                System.out.println(rent + " rent is paid to " + getOwner().getPiece().getName());
+            }
 
     }
 
-    public int getPrice() {
+    private int getPrice() {
         return price;
     }
 
-    public Player getOwner() {
+    private Player getOwner() {
         return owner;
     }
 
-    public Player setOwner(Player p) {
-        return p;
+    private void setOwner(Player p) {
+        owner = p;
     }
 
 
